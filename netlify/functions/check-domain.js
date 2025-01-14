105326d082a059bb03a3d73bbd2a5d834ce514fd
lion-ilustre <74267850+lionilustres@users.noreply.github.com>
2025-01-13 22:45:24 -0500

    update-app

--- netlify/functions/check-domain.js
+++ netlify/functions/check-domain.js
@@ -1,23 +1,23 @@
 const axios = require('axios');
 
 exports.handler = async (event) => {
-    const domain = event.queryStringParameters.domain;
-    const apiKey = process.env.API_KEY; // Obtener la clave API desde una variable de entorno
+  const domain = event.queryStringParameters.domain;
+  const apiKey = process.env.API_KEY; // Obtener la clave API desde una variable de entorno
 
-    try {
-        const response = await axios.get(`https://api.apilayer.com/whois/query?domain=${domain}`, {
-            headers: {
-                'apikey': apiKey
-            }
-        });
-        return {
-            statusCode: 200,
-            body: JSON.stringify(response.data)
-        };
-    } catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ error: 'Error al verificar el dominio' })
-        };
-    }
+  try {
+    const response = await axios.get(`https://api.apilayer.com/whois/query?domain=${domain}`, {
+      headers: {
+        'apikey': apiKey
+      }
+    });
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.data)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Error al verificar el dominio' })
+    };
+  }
 };
