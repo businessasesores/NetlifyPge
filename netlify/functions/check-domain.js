c58a9b640cbb2741ddebe3f8ad9e32192aba1e06
lion-ilustre <74267850+lionilustres@users.noreply.github.com>
2025-01-19 20:40:11 -0500

    update

--- netlify/functions/check-domain.js
+++ netlify/functions/check-domain.js
@@ -1,33 +1,20 @@
 const axios = require('axios');
 
 exports.handler = async (event) => {
-  console.log('Event object:', JSON.stringify(event, null, 2)); 
-
-  // Verificar si el objeto 'event' y sus propiedades existen
-  if (!event || !event.request || !event.request.headers) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Invalid event format' })
-    };
-  }
+  console.log('Event object:', JSON.stringify(event, null, 2)); // Log the event object for debugging
 
   // Verificar el origen de la solicitud (restringe el acceso al origen permitido)
   const allowedOrigin = 'https://buscador.hostweb.workers.dev'; // Reemplaza con tu origen permitido
-  const origin = event.request.headers.get('Origin'); 
-  if (origin !== allowedOrigin) {
+  const origin = event.headers.get('Origin');
+  if (!event || !event.request || !event.request.headers || origin !== allowedOrigin) {
     return {
       statusCode: 403,
-      body: JSON.stringify({ error: 'Forbidden: Request origin not allowed' })
+      body: JSON.stringify({ error: 'Forbidden: Request origin not allowed' }),
     };
   }
 
-  // Verificar la autenticación (opcional)
-  // ... (código para autenticación)
-
   // Extraer el dominio de los parámetros de la cadena de consulta
   const domain = event.queryStringParameters.domain;
-
-  // Validación opcional del dominio (agrega lógica si es necesario)
 
   // Realizar la solicitud a la API de Whois usando Axios
   try {
@@ -43,10 +30,10 @@ exports.handler = async (event) => {
       body: JSON.stringify(response.data),
     };
   } catch (error) {
-    console.error('Error verifying domain:', error);
+    console.error('Error al verificar el dominio:', error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: 'Error verifying domain' }),
+      body: JSON.stringify({ error: 'Error al verificar el dominio' }),
     };
   }
 };
