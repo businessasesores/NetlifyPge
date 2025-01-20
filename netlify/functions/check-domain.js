6572f83e0f50afdb5e1998911464136eb3d0a43b
lion-ilustre <74267850+lionilustres@users.noreply.github.com>
2025-01-19 21:25:02 -0500

    lo

--- netlify/functions/check-domain.js
+++ netlify/functions/check-domain.js
@@ -1,40 +1,48 @@
 const axios = require('axios');
 
 exports.handler = async (event) => {
+  console.log('Event object:', JSON.stringify(event, null, 2));
+
+  // Verificar el origen de la solicitud (restringe el acceso al origen permitido)
+  const allowedOrigin = 'https://buscador.hostweb.workers.dev';
+  const origin = event.headers.get('Origin');
+  if (!event || !event.request || !event.request.headers || origin !== allowedOrigin) {
+    return {
+      statusCode: 403,
+      body: JSON.stringify({ error: 'Forbidden: Request origin not allowed' })
+    };
+  }
+
+  // Verificar el token de autenticación (opcional, agrega si es necesario)
+  // const authToken = event.headers.get('Authorization');
+  // const expectedToken = process.env.AUTH_TOKEN;
+  // if (authToken !== `Bearer ${expectedToken}`) {
+  //   return {
+  //     statusCode: 401,
+  //     body: JSON.stringify({ error: 'Unauthorized' })
+  //   };
+  // }
+
+  // Extraer el dominio de los parámetros de la cadena de consulta
   const domain = event.queryStringParameters.domain;
-  const apiKey = process.env.API_KEY; // Obtener la clave API desde una variable de entorno
-  // Verificar el origen de la solicitud
-const allowedOrigin = 'https://buscador.hostweb.workers.dev';
-const origin = event.headers.get('Origin');
-if (origin !== allowedOrigin) {
-  return {
-    statusCode: 403,
-    body: JSON.stringify({ error: 'Forbidden: Request origin not allowed' })
-  };
-}
-
-// Verificar el token de autenticación
-const authToken = event.headers.get('Authorization');
-const expectedToken = process.env.AUTH_TOKEN;
-if (authToken !== `Bearer ${expectedToken}`) {
-  return {
-    statusCode: 401,
-    body: JSON.stringify({ error: 'Unauthorized' })
-  };
-}
-
 
   try {
+    const apiKey = process.env.API_KEY;
     const response = await axios.get(`https://api.apilayer.com/whois/query?domain=${domain}`, {
       headers: {
         'apikey': apiKey
       }
     });
+
     return {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': allowedOrigin
+      },
       body: JSON.stringify(response.data)
     };
   } catch (error) {
+    console.error('Error al verificar el dominio:', error);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: 'Error al verificar el dominio' })
