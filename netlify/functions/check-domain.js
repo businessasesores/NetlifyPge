cc6936e155f913e05b46aceec5f7375d4470f13d
lion-ilustre <74267850+lionilustres@users.noreply.github.com>
2025-01-20 18:08:05 -0500

    ffgfhhghgh

--- netlify/functions/check-domain.js
+++ netlify/functions/check-domain.js
@@ -1,42 +1,56 @@
-
 const axios = require('axios');
 
 exports.handler = async (event) => {
   const domain = event.queryStringParameters.domain;
-  const apiKey = process.env.API_KEY; // Obtener la clave API desde una variable de entorno
-   const origin = event.request?.headers?.get('Origin'); // Usamos el operador de encadenamiento opcional para evitar errores si headers es undefined
-    const allowedOrigin = 'https://buscador.hostweb.workers.dev';
+  const apiKey = process.env.API_KEY;
 
+  // CORS validation
+  const origin = event.headers.get('Origin');
+  const allowedOrigin = 'https://buscador.hostweb.workers.dev';
+
+  if (origin !== allowedOrigin) {
+    return {
+      statusCode: 403,
+      body: JSON.stringify({ error: 'Solicitud no autorizada: Origen no permitido' })
+    };
+  }
 
   try {
     const response = await axios.get(`https://api.apilayer.com/whois/query?domain=${domain}`, {
       headers: {
         'apikey': apiKey
-      },
-      timeout: 5000
+      }
     });
 
-    
-
-      // CORS validation (optional, assuming CORS is configured on Netlify)
-    if (origin !== allowedOrigin) {
+    if (response.status === 200) {
+      // Verificar el estado del dominio
+      if (response.data.status === 'registered') {
+        return {
+          statusCode: 200,
+          body: JSON.stringify({ message: 'El dominio está registrado' })
+        };
+      } else if (response.data.status === 'available') {
+        return {
+          statusCode: 200,
+          body: JSON.stringify({ message: 'El dominio está disponible' })
+        };
+      } else {
+        return {
+          statusCode: 400,
+          body: JSON.stringify({ error: 'Respuesta de la API no válida' })
+        };
+      }
+    } else {
       return {
-        statusCode: 403,
-        body: JSON.stringify({ error: 'Solicitud no autorizada: Origen no permitido' }),
+        statusCode: response.status,
+        body: JSON.stringify({ error: 'Error al obtener datos de Whois' })
       };
     }
-
-return {
-       statusCode: 200,
-      body: JSON.stringify(response.data === 'registered')
-      
-    };
-    
-  } catch (result) {
+  } catch (error) {
+    console.error('Error:', error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ result : 'el dominio esta disponible?'  === 'registered'})
+      body: JSON.stringify({ error: 'Error interno del servidor' })
     };
   }
 };
-
