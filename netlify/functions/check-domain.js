9dc20cf65f4ffd4d0af2b0de5ee67a559cbff633
lion-ilustre <74267850+lionilustres@users.noreply.github.com>
2025-01-20 13:57:04 -0500

    ttrrr

--- netlify/functions/check-domain.js
+++ netlify/functions/check-domain.js
@@ -1,38 +1,27 @@
-
 const axios = require('axios');
 
 exports.handler = async (event) => {
-  const domain = event.queryStringParameters.domain;
-  const apiKey = process.env.API_KEY; // Obtener la clave API desde una variable de entorno
-   const origin = event.request?.headers?.get('Origin'); // Usamos el operador de encadenamiento opcional para evitar errores si headers es undefined
-    const allowedOrigin = 'https://buscador.hostweb.workers.dev';
-
-
   try {
-    const response = await axios.get(`https://api.apilayer.com/whois/query?domain=${domain}`, {
+    const { domain } = event.queryStringParameters;
+    const apiKey = process.env.API_KEY;
+    const apiUrl = `https://api.apilayer.com/whois/query?domain=${domain}`;
+
+    const response = await axios.get(apiUrl, {
       headers: {
         'apikey': apiKey
-      }
+      },
+      timeout: 5000
     });
 
-    if (origin !== allowedOrigin) {
-      return {
-        statusCode: 200,
-      body: JSON.stringify(response.data)
-        
-      };
-    }
-
     return {
-      
-      statusCode: 403,
-        body: JSON.stringify({ error: 'Solicitud no autorizada: Origen no permitido' })
+      statusCode: 200,
+      body: JSON.stringify(response.data)
     };
   } catch (error) {
+    console.error('Error:', error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: 'Error al verificar el dominio' })
+      body: JSON.stringify({ error: 'Error al procesar la solicitud' })
     };
   }
 };
-
