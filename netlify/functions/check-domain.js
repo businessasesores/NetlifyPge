95e616f780775e7194c9fd22b4736dc99fc57e58
lion-ilustre <74267850+lionilustres@users.noreply.github.com>
2025-01-19 22:27:05 -0500

    Update check-domain.js
    newpost

--- netlify/functions/check-domain.js
+++ netlify/functions/check-domain.js
@@ -3,6 +3,7 @@ const axios = require('axios'); // Import axios at the module level
 exports.handler = async (event) => {
   console.log('Event object:', JSON.stringify(event, null, 2));
 
+  // Verify event object and its properties
   if (!event || !event.request || !event.request.headers) {
     return {
       statusCode: 500,
@@ -13,6 +14,7 @@ exports.handler = async (event) => {
   const origin = event.request.headers.get('Origin');
   const allowedOrigin = 'https://buscador.hostweb.workers.dev';
 
+  // Check origin and return error if not allowed
   if (origin !== allowedOrigin) {
     return {
       statusCode: 403,
@@ -20,20 +22,24 @@ exports.handler = async (event) => {
     };
   }
 
+  // Extract domain from query parameters
   const domain = event.queryStringParameters.domain;
 
   try {
-    const apiKey = process.env.API_KEY;
+    const apiKey = process.env.API_KEY; // Ensure API key is set as an environment variable
+
+    // Make the request to the Whois API using Axios
     const response = await axios.get(`https://api.apilayer.com/whois/query?domain=${domain}`, {
       headers: {
         'apikey': apiKey
       }
     });
 
+    // Handle successful response
     return {
       statusCode: 200,
       headers: {
-        'Access-Control-Allow-Origin': allowedOrigin
+        'Access-Control-Allow-Origin': allowedOrigin // Allow CORS
       },
       body: JSON.stringify(response.data)
     };
