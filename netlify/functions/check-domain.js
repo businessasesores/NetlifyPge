e08c8e95ca697ab88f1d4fc9c0ff5857c599a330
lion-ilustre <74267850+lionilustres@users.noreply.github.com>
2025-01-19 21:20:24 -0500

    proveComiit

--- netlify/functions/check-domain.js
+++ netlify/functions/check-domain.js
@@ -3,6 +3,26 @@ const axios = require('axios');
 exports.handler = async (event) => {
   const domain = event.queryStringParameters.domain;
   const apiKey = process.env.API_KEY; // Obtener la clave API desde una variable de entorno
+  // Verificar el origen de la solicitud
+const allowedOrigin = 'https://buscador.hostweb.workers.dev';
+const origin = event.headers.get('Origin');
+if (origin !== allowedOrigin) {
+  return {
+    statusCode: 403,
+    body: JSON.stringify({ error: 'Forbidden: Request origin not allowed' })
+  };
+}
+
+// Verificar el token de autenticación
+const authToken = event.headers.get('Authorization');
+const expectedToken = process.env.AUTH_TOKEN;
+if (authToken !== `Bearer ${expectedToken}`) {
+  return {
+    statusCode: 401,
+    body: JSON.stringify({ error: 'Unauthorized' })
+  };
+}
+
 
   try {
     const response = await axios.get(`https://api.apilayer.com/whois/query?domain=${domain}`, {
