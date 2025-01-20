1c86b9fdd28c1a451f61671831c987c10993fb66
lion-ilustre <74267850+lionilustres@users.noreply.github.com>
2025-01-20 14:41:26 -0500

    gfhghgh

--- netlify/functions/check-domain.js
+++ netlify/functions/check-domain.js
@@ -1,39 +1,43 @@
-
 const axios = require('axios');
 
 exports.handler = async (event) => {
-  const domain = event.queryStringParameters.domain;
-  const apiKey = process.env.API_KEY; // Obtener la clave API desde una variable de entorno
-   const origin = event.request?.headers?.get('Origin'); // Usamos el operador de encadenamiento opcional para evitar errores si headers es undefined
-    const allowedOrigin = 'https://buscador.hostweb.workers.dev';
-
-
   try {
-    const response = await axios.get(`https://api.apilayer.com/whois/query?domain=${domain}`, {
+    const { domain } = event.queryStringParameters;
+    const apiKey = process.env.API_KEY;
+    const apiUrl = `https://api.apilayer.com/whois/query?domain=${domain}`;
+
+    const response = await axios.get(apiUrl, {
       headers: {
         'apikey': apiKey
       },
       timeout: 5000
     });
 
-  return {
-      statusCode: 200,
-      body: JSON.stringify(response.data)
-    };
-
-        if (origin !== allowedOrigin) {
+    if (response.status === 200) {
+      // Verificar si el dominio está registrado
+      if (response.data.result === 'registered') {
+        return {
+          statusCode: 200,
+          body: JSON.stringify({ message: 'El dominio está registrado' })
+        };
+      } else {
+        return {
+          statusCode: 404,
+          body: JSON.stringify({ message: 'El dominio no está registrado' })
+        };
+      }
+    } else {
+      console.error('Error en la API de Whois:', response.data);
       return {
-        statusCode: 403,
-        body: JSON.stringify({ error: 'Solicitud no autorizada: Origen no permitido' }),
+        statusCode: response.status,
+        body: JSON.stringify({ error: 'Error al obtener datos de Whois', details: response.data })
       };
     }
-
-
   } catch (error) {
-    console.error('Error:', error);
+    console.error('Error al realizar la solicitud:', error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: 'Error al procesar la solicitud' })
+      body: JSON.stringify({ error: 'Error interno del servidor' })
     };
-  };
+  }
 };
