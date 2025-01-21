196d0991be14fede138c61047fd367aa6474285e
lion-ilustre <74267850+lionilustres@users.noreply.github.com>
2025-01-20 22:37:50 -0500

    errerer

--- netlify/functions/check-domain.js
+++ netlify/functions/check-domain.js
@@ -1,47 +1,48 @@
- const axios = require('axios');
+
+const axios = require('axios');
 
 exports.handler = async (event) => {
   const domain = event.queryStringParameters.domain;
-  const apiKey = process.env.API_KEY; // Obtain API key from an environment variable
+  const apiKey = process.env.API_KEY; // Obtener la clave API desde una variable de entorno
+   const origin = event.request?.headers?.get('Origin'); // Usamos el operador de encadenamiento opcional para evitar errores si headers es undefined
+    const allowedOrigin = 'https://buscador.hostweb.workers.dev';
 
-  // Declare and assign the allowedOrigin variable
-  const allowedOrigin = 'https://businessasesores.web.app'; 
-
-  // Access headers using the new event structure
-  const origin = event.headers['origin']; 
-
-  if (origin !== allowedOrigin) {
-    return {
-      statusCode: 403,
-      body: JSON.stringify({ error: 'Solicitud no autorizada: Origen no permitido' })
-    };
-  }
 
   try {
     const response = await axios.get(`https://api.apilayer.com/whois/query?domain=${domain}`, {
       headers: {
-        'apikey': apiKey,
+        'apikey': apiKey
       },
-      timeout: 5000, // Set a timeout for the API request
+      timeout: 5000
     });
 
-  if (response.status === 200) {
-  // Ajusta esta parte según la estructura de la respuesta de tu API de Whois
-  if (response.data.status === 'registered') {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'El dominio está registrado' })
+      // CORS validation (optional, assuming CORS is configured on Netlify)
+    if (origin !== allowedOrigin) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: 'Solicitud no autorizada: Origen no permitido' }),
+      };
     };
-  } else (message) {
-    return  {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'El dominio está disponible' })
+
+return {
+       statusCode: 200,
+      body: JSON.stringify(response.data)
+      
     };
-  }
-} catch (message) {
+
+     return  {
+          statusCode: 200,
+          body: JSON.stringify({ result: 'El dominio está disponible' (response.data.result === 'registered') })
+        };  
+
+    
+
+    
+  } catch (message) {
     return {
       statusCode: 500,
       body: JSON.stringify({ message : 'el dominio esta disponible?' })
     };
   }
 };
+
