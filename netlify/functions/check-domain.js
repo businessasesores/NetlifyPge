a5a4b47ce92df813b65d4703ef77717e787de123
lion-ilustre <74267850+lionilustres@users.noreply.github.com>
2025-01-20 22:28:12 -0500

    gfghhhgjhjhjh

--- netlify/functions/check-domain.js
+++ netlify/functions/check-domain.js
@@ -43,14 +43,6 @@ exports.handler = async (event) => {
         };
       }
 
-
-     return  {
-          statusCode: 200,
-          body: JSON.stringify({ result: 'El dominio está disponible' (response.data.result === 'registered') })
-        };  
-
-    
-
     
   } catch (message) {
     return {
