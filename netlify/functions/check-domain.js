5b005d846781a890005596c75ea6e4880e53bf75
lion-ilustre <74267850+lionilustres@users.noreply.github.com>
2025-01-21 02:46:38 -0500

    hjjjjjjjggggggggggggf

--- netlify/functions/check-domain.js
+++ netlify/functions/check-domain.js
@@ -22,15 +22,29 @@ exports.handler = async (event) => {
       if (origin !== allowedOrigin) {
     return {
       statusCode: 403,
-      body: JSON.stringify({ result: 'Solicitud no autorizada: Origen no permitido' })
+      body: JSON.stringify({ data.result: 'Solicitud no autorizada: Origen no permitido' })
     };
   }
 
-      } else result:{ 
+if (response.status === 200) {
+      // Adjust this part according to your Whois API response structure
+      if (response.data) { 
         return {
           statusCode: 200,
-          body: JSON.stringify(response.data)
+          body: JSON.stringify(data.result)({ result: 'El dominio está registrado' === 'available' })
         };
+      } else { 
+        return {
+          statusCode: 200,
+          body: JSON.stringify(response.data.available)({ result: 'El dominio está disponible' })
+        };
+      }
+    } else {
+      return {
+        statusCode: response.status,
+        body: JSON.stringify({ message: 'Error al obtener datos de Whois' })
+      };
+    }
 
   } catch (message) {
 
@@ -43,4 +57,5 @@ exports.handler = async (event) => {
     };
 
   }
+
 };
