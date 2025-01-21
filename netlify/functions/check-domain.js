f44853328467027c7ca0ba9feeff153bc999369a
lion-ilustre <74267850+lionilustres@users.noreply.github.com>
2025-01-21 00:05:31 -0500

    hgjgj

--- netlify/functions/check-domain.js
+++ netlify/functions/check-domain.js
@@ -1,51 +1,44 @@
 const axios = require('axios');
 
 exports.handler = async (event) => {
-
   const domain = event.queryStringParameters.domain;
-  const apiKey = process.env.API_KEY; 
-  const origin = event.request?.headers?.get('Origin'); 
-  const allowedOrigin = 'https://businessasesores.web.app';
+  const apiKey = process.env.API_KEY; // Obtain API key from an environment variable
 
- try {
-
+  try {
     const response = await axios.get(`https://api.apilayer.com/whois/query?domain=${domain}`, {
-
       headers: {
-
         'apikey': apiKey
-
       },
-
-      timeout: 5000
-
+      timeout: 5000 // Set a timeout for the API request
     });
 
-    if (origin !== allowedOrigin) {
-
+    if (response.status === 200) {
+      // Adjust this part according to your Whois API response structure
+      if (!response.data) {
+        // Handle empty response (e.g., domain not found)
+        return {
+          statusCode: 200,
+          body: JSON.stringify({ message: 'El dominio no se encontró o hubo un error en la consulta' })
+        };
+      } else {
+        const isAvailable = response.data.status !== 'registered'; // Assuming 'registered' indicates unavailable
+        return {
+          statusCode: 200,
+          body: JSON.stringify({ message: isAvailable ? 'El dominio está disponible' : 'El dominio está registrado' })
+        };
+      }
+    } else {
+      console.error('Error en la API de Whois:', response.data);
       return {
-
-        statusCode: 403,
-
-        body: JSON.stringify({ error: 'Solicitud no autorizada: Origen no permitido' })
-
+        statusCode: response.status,
+        body: JSON.stringify({ error: 'Error al obtener datos de Whois', details: response.data })
       };
-
     }
-
+  } catch (error) {
+    console.error('Error:', error);
     return {
-
-      statusCode: 200,
-
-      body: JSON.stringify(response.data)
-
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Error interno del servidor' })
     };
-  } catch (message) {
-    return {
-    statusCode: 500,
-    body: JSON.stringify({ message: 'el dominio esta disponible?' })
-    };
-
   }
-
 };
