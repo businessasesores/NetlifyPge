7c78382455923ea8c4ffb5f86defd96cfbf1caad
lion-ilustre <74267850+lionilustres@users.noreply.github.com>
2025-01-22 12:15:36 -0500

    ghhgghgh

--- netlify/functions/check-domain.js
+++ netlify/functions/check-domain.js
@@ -2,47 +2,28 @@ const axios = require('axios');
 
 exports.handler = async (event) => {
   const domain = event.queryStringParameters.domain;
-  const apiKey = process.env.API_KEY; // Obtienes tu API Key de un archivo de configuración
-  const allowedOrigin = 'https://businessasesores.web.app'; // Tu dominio permitido
-  const origin = event.headers.origin;
+  const apiKey = process.env.API_KEY;
 
-  // Manejo de CORS
-  if (origin !== allowedOrigin) {
+  if (!domain) {
     return {
-      statusCode: 403,
-      headers: {
-        'Access-Control-Allow-Origin': allowedOrigin,
-        'Access-Control-Allow-Methods': 'GET, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type',
-      },
-      body: JSON.stringify({ error: 'Solicitud no autorizada: Origen no permitido.' }),
+      statusCode: 400,
+      body: JSON.stringify({ error: 'El parámetro "domain" es obligatorio.' }),
     };
   }
 
   try {
-    // Hacemos la solicitud a la API de apilayer para consultar el dominio
     const response = await axios.get(`https://api.apilayer.com/whois/check?domain=${domain}`, {
       headers: { apikey: apiKey },
     });
 
-    // Retornamos la respuesta con la información
     return {
       statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': allowedOrigin,
-        'Content-Type': 'application/json',
-      },
       body: JSON.stringify(response.data),
     };
   } catch (error) {
-    // Si el dominio no se encuentra o hay un error
     return {
       statusCode: 500,
-      headers: {
-        'Access-Control-Allow-Origin': allowedOrigin,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ error: 'Error en la consulta de Whois. El dominio no fue encontrado o hubo un problema.' }),
+      body: JSON.stringify({ error: 'Error en el servidor o en la consulta.' }),
     };
   }
 };
