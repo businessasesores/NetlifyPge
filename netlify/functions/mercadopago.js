8166f37fc722dfd0d0be7cdbcc9f6b793f2e3cb0
lion-ilustre <74267850+lionilustres@users.noreply.github.com>
2025-02-01 12:27:08 -0500

    fggfhghh

--- netlify/functions/mercadopago.js
+++ netlify/functions/mercadopago.js
@@ -1,36 +1,25 @@
-// Importamos el SDK de MercadoPago
 const mercadopago = require('mercadopago');
 
-// Usamos directamente el token en lugar de la variable de entorno
-mercadopago.configurations.setAccessToken('APP_USR-7637184600887888-040602-c75180a883ad24b9c38576741213cb60-1040849203'); // Asegúrate de usar tu token
-
 exports.handler = async (event, context) => {
+  mercadopago.configure({
+    access_token: process.env.MERCADO_PAGO_ACCESS_TOKEN
+  });
+
+  const { body } = event;
+  const paymentData = JSON.parse(body);
+
   try {
-    // Crear una preferencia de pago para la prueba
-    const preference = {
-      items: [
-        {
-          title: 'Test Item',
-          quantity: 1,
-          unit_price: 100.0,
-        },
-      ],
-    };
+    const payment = await mercadopago.payments.create(paymentData);
 
-    const response = await mercadopago.preferences.create(preference);
-
-    // Si todo está bien, devolvemos el ID de la preferencia creada
     return {
       statusCode: 200,
-      body: JSON.stringify({
-        id: response.body.id,
-      }),
+      body: JSON.stringify(payment)
     };
   } catch (error) {
-    console.error("Error al crear la preferencia", error);
+    console.error("Error en la función de Mercado Pago:", error); // Imprime el error completo en la consola
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: 'Error creando la preferencia', error: error.message }),
+      body: JSON.stringify({ error: error.message, details: error }) // Devuelve un mensaje de error más detallado
     };
   }
 };
